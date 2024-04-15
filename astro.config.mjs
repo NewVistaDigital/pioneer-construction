aec9de39279b74645081069831cc6f098d6d8b92
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-04-15 15:54:06 -0600

    Create contact us page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon(), tailwind(), robotsTxt()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        solar: [
+          "letter-bold",
+          "phone-calling-bold"
+        ]
+      }
+    }),
+    tailwind(),
+    robotsTxt()],
   output: "static",
   adapter: netlify(),
 });
