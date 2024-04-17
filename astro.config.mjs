74f3767515b780b2ff1720160a59b68fe884490d
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-04-16 20:56:06 -0600

    Create services and projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,8 @@ export default defineConfig({
         solar: [
           "letter-bold",
           "phone-calling-bold"
-        ]
+        ],
+        ic: ["*"]
       }
     }),
     tailwind(),
