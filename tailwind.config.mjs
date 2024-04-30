a3e9b00871b81d1cf7a64625bd834d55b85207fc
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-04-30 17:47:06 -0600

    fix: add target to button for homepage contest component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@ export default {
     },
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
-      serif: ["Merriweather", "serif"],
+      serif: ["Space Grotesk", "serif"],
     },
   },
   plugins: [require("@tailwindcss/typography")],
